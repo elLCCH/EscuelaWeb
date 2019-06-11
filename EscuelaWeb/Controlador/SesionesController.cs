37668add56ab9c7c11396c31ff2384b441fa65e1
Luis Choque <cristian.98oruro@gmail.com>
2019-06-11 00:38:07 -0400

    idea de login usando un controlador INGRESO SOLO PARA INTERESADO

---
+++ EscuelaWeb/Controlador/SesionesController.cs
@@ -0,0 +1,45 @@
+﻿using EscuelaWeb.Data;
+using EscuelaWeb.Data.dsEscuelaTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EscuelaWeb.Controlador
+{
+    public class SesionesController
+    {
+        AdministrativoTableAdapter admin = new AdministrativoTableAdapter();
+        EstudianteTableAdapter est = new EstudianteTableAdapter();
+        ProfesorTableAdapter prof = new ProfesorTableAdapter();
+        dsEscuela _ds = new dsEscuela();
+        private int ci_estudiante;
+        private string nombre;
+        private string ap_paterno;
+        private string ap_materno;
+        private string contrasenia;
+        private int celular;
+        private DateTime fecha_nac;
+        private string direccion;
+        private int id_curso;
+
+        public int Ci_estudiante { get => ci_estudiante; set => ci_estudiante = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Ap_paterno { get => ap_paterno; set => ap_paterno = value; }
+        public string Ap_materno { get => ap_materno; set => ap_materno = value; }
+        public string Contrasenia { get => contrasenia; set => contrasenia = value; }
+        public int Celular { get => celular; set => celular = value; }
+        public DateTime Fecha_nac { get => fecha_nac; set => fecha_nac = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public int Id_curso { get => id_curso; set => id_curso = value; }
+
+
+        public bool AutenticarEstudiante(int pCuenta, string pClave)
+        {
+            if (est.FillByAutenticarEstudiante(_ds.Estudiante, pCuenta, pClave) > 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
--- EscuelaWeb/Vistas/index.aspx.cs
+++ EscuelaWeb/Vistas/index.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EscuelaWeb.Controlador;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,20 +10,31 @@ namespace EscuelaWeb.Vistas
 {
     public partial class index : System.Web.UI.Page
     {
+        SesionesController SC = new SesionesController();
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
         protected void btnIniciar_Click(object sender, EventArgs e)
         {
-            if (cbValidar.Checked==true)
+            if (txtContrasenia.Text != "" && txtCuenta.Text != "")
             {
-                //Response.Redirect("Secretaria/frmBSecretaria.aspx");
-                //Response.Redirect("Profesor/frmBProfesor.aspx");
-                Response.Redirect("Interesado/frmBInteresado.aspx");
-                //Response.Redirect("Director_UE/frmBDirector.aspx");
+                if (SC.AutenticarEstudiante(Convert.ToInt32(txtCuenta.Text), txtContrasenia.Text) == true)
+                {
+                    //Response.Redirect("Secretaria/frmBSecretaria.aspx");
+                    //Response.Redirect("Profesor/frmBProfesor.aspx");
+                    Response.Redirect("Interesado/frmBInteresado.aspx");
+                    //Response.Redirect("Director_UE/frmBDirector.aspx");
+                }
+                else
+                {
+                    
+                }
+                Response.Redirect("index.aspx");
             }
+            //Response.Redirect("index.aspx");
             
+
         }
     }
 }
